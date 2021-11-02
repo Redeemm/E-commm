641558c109719dc197b8ecfaa81aa5d528fe4b42
Redeemm <redeemerdela419@gmail.com>
2021-11-02 15:52:54 +0000

    More addups 😂🔥🔥

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import AppNavigator from './src/navigator/navigator'
+import {AppLoading} from 'expo'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hwllo World! 😎</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    isFontLoaded : false
+  }
+  async componentDidMount(){
+    await Font.loadAsync({
+      'Bold' : require('./src/fonts/Montserrat-ExtraBold.ttf'),
+      'Medium' : require('.src/fonts/Montserrat-Medium.ttf'),
+      'Regular' : require('.src/fonts/Montserrat-Regular.ttf'),
+    });
+    this.setState({isFontLoaded : true})
+  }
+  render() {
+    return(
+      (this.state.isFontLoaded === true)? (<AppNavigator/>):(<AppNavigator/>)
+    );
+  }
+    
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
